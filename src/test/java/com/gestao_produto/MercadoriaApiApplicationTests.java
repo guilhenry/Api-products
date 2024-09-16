0f6665e7d5e4ccd65024dc0327f729d816f30c98
guilhenry <guilhermihenry@gmail.com>
2024-09-16 20:49:38 -0300

    commit inicial e configuração do h2

---
+++ src/test/java/com/gestao_produto/MercadoriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
