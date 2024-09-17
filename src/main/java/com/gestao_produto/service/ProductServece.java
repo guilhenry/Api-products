a2d258f90d27a00a2c8ef5407a7f7559d789fae2
guilhenry <guilhermihenry@gmail.com>
2024-09-17 19:40:36 -0300

    criação do servece

---
+++ src/main/java/com/gestao_produto/service/ProductServece.java
@@ -0,0 +1,40 @@
+package com.gestao_produto.service;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+public class ProductServece {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal preco;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
